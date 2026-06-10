d3c313b8977e542630ae9ff4fd3c2e3eb6ea853a
usanema <nowik.tools@gmail.com>
2026-06-10 16:10:48 +0200

    feat: implement SEO optimization with OpenGraph, JSON-LD, and Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,12 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: 'https://weronikanowik.pl',
+  integrations: [react(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()]
