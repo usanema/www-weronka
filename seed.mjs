ee38cc49c52638bac4592ab00a6d3d19b134b86d
usanema <nowik.tools@gmail.com>
2026-06-10 09:52:30 +0200

    chore: change PocketBase URL to srv50.mikr.us

--- seed.mjs
+++ seed.mjs
@@ -1,6 +1,6 @@
 import PocketBase from 'pocketbase';
 
-const pb = new PocketBase('http://127.0.0.1:8090');
+const pb = new PocketBase('http://srv50.mikr.us:30154');
 
 async function createCollectionSafe(collectionConfig) {
   try {
